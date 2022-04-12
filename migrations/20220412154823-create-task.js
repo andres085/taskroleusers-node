32f3478390b96e273000bda2d46ec7e12293d117
andres085 <andresito.martinez1985@gmail.com>
2022-04-12 13:04:56 -0300

    Primer commit con todo instalado y modelos y migraciones armadas

---
+++ migrations/20220412154823-create-task.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tasks');
+  }
+};
---
+++ models/task.js
@@ -0,0 +1,22 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Task extends Model {
+    static associate(models) {
+      Task.belongsToMany(models.User, {
+        foreignKey: "userId",
+      })
+    }
+  }
+  Task.init({
+    title: DataTypes.STRING,
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    paranoid: true,
+    modelName: 'Task',
+  });
+  return Task;
+};
